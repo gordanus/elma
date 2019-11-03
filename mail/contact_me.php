1343f4e92b11c441d1819a55a749ed1e7f8e28a2
Gordan Blazic <gordanus@gmail.com>
2019-11-03 13:32:47 +0100

    mail service update

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'gordanus@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Poruka sa sajta od strane:  $name";
-$email_body = "Stigla je nova poruka sa sajta NN Roštilj.\n\n"."Detalji poruke:\n\nIme: $name\n\nEmail: $email_address\n\nTekst poruke:\n$message";
+$email_body = "Stigla je nova poruka sa sajta ELMA.\n\n"."Detalji poruke:\n\nIme: $name\n\nEmail: $email_address\n\nTekst poruke:\n$message";
 $headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body, $headers);
